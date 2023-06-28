a1df078ed101f0a612e9dfafa70bb39901392b27
Celestin Cordos <celestin.cordos@outlook.fr>
2023-06-28 21:57:29 +0200

    Will have to check that these names work well on mac and linux

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
-# TODO...
+#!/bin/bash
+
+# Source folder path
+source_folder="./files"
+
+# Destination folder path
+destination_folder="~"
+
+# Copy files from source folder to destination folder
+cp -r "$source_folder"/* "$destination_folder"
+
+# Print success message
+echo "Files copied successfully!"
