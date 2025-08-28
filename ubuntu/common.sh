f5bcf3f407f0dad5d067f442eb6a7374c63765b8
Célestin Cordos <celestin.cordos@outlook.fr>
2025-08-28 13:56:55 +0200

    Apt instead of nala

--- ubuntu/common.sh
+++ ubuntu/common.sh
@@ -1,7 +1,7 @@
 
 # Nala
-sudo apt install nala
-sudo nala update
+sudo apt install apt
+sudo apt update
 sudo apt install curl wget -y texlive-xetex texlive-fonts-recommended texlive-plain-generic
 
 
