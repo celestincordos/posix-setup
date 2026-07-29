c3048beaf37df7ce3be0d2e97eb72f212dbe9359
Célestin Cordos <celestin.cordos@outlook.fr>
2026-07-29 15:31:35 +0200

    stenv, git commands and  sort files in folder by date

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -10,5 +10,14 @@ DESTINATION_FOLDER=$HOME
 # Copy files from source folder to destination folder
 cp -r dotfiles/.* "$DESTINATION_FOLDER"
 
+# Install personal commands
+mkdir -p "$HOME/.local/bin"
+if [ -d "$SOURCE_FOLDER/.local/bin" ]; then
+    for command in "$SOURCE_FOLDER"/.local/bin/*; do
+        [ -f "$command" ] || continue
+        install -m 755 "$command" "$HOME/.local/bin/"
+    done
+fi
+
 # Print success message
 echo "Dotfiles copied successfully!"
